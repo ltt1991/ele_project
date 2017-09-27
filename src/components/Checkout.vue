b5f3c508eae2292524c83d6ed156a816cbdd7c35
ltt1991 <1353343921@qq.com>
2017-09-27 14:54:33 +0800

    discovery add imgs and router before

---
+++ src/components/Checkout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="checkout">
+       
+    </div>
+</template>
+    
+<script>
+export default {
+  name: "component_name",
+  data () {
+    return {
+        
+    };
+  }
+}
+</script>
+    
+<style lang="css" scoped>
+    
+</style>
